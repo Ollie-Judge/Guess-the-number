ea3cba7b417bda9b7ba2f3e3d8a291a1649c5f10
Ollie-Judge <ollie.judge@hotmail.com>
2022-12-13 16:36:54 +0000

    added more reusable code for the function

--- script.js
+++ script.js
@@ -19,8 +19,12 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "💥 You lost the game!";
     }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 Too low!";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
   }
 });
