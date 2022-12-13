5ee5941d05039ab739ce540656697dc6de7e5486
Ollie-Judge <ollie.judge@hotmail.com>
2022-12-13 16:05:46 +0000

    added an if that looks at if score is more than 1 then do loose game or too high

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let score = 20;
+
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
@@ -9,8 +11,16 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 Too high!";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "📉 Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
